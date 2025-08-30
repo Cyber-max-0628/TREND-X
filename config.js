28d01b1b2183e4cd277ccd8a0bd20b8a943dea1f
Cyber-max-0628 <vertigopoiuylkjhg@gmail.com>
2025-08-30 18:25:41 +0300

    Update config.js

--- config.js
+++ config.js
@@ -48,7 +48,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "254734939236",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "254748458310",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "TRENDEX",
 // add bot owner name
